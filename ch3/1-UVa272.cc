2ea64bd961111624515ea5d6d39d5a57e7344150
Baiyu Zhao <csbaiyu@gmail.com>
2016-01-31 21:16:34 +0800

    1-Uva272

---
+++ ch3/1-UVa272.cc
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <string>
+using std::cin;
+using std::cout;
+using std::endl;
+using std::string;
+int main() {
+  string input_line;
+  bool quote_flag = false; 
+  while (getline(cin, input_line)) {
+    for (char input_ch : input_line)
+      if (input_ch == '\"' && quote_flag == 0) {
+        cout << "``";
+        quote_flag = 1;
+      } else if (input_ch == '\"' && quote_flag == 1) {
+        cout << "\'\'";
+        quote_flag = 0;
+      } else {
+        cout << input_ch;
+      }
+    cout << endl;
+  }
+  return 0;
+}
